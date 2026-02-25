d26172f56f60463c1c8d5254800adbfb3e5cae18
Fawad ALi <fawadalichaudhary97@gmail.com>
2026-02-25 15:55:44 +0500

    fadi

---
+++ script.js
@@ -0,0 +1,82 @@
+const btn = document.getElementById('num1')
+
+btn.addEventListener("click", function () {
+    const btn1 = btn.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num = document.getElementById('num2')
+
+num.addEventListener("click", function () {
+    const btn1 = num.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num1 = document.getElementById('num3')
+
+num1.addEventListener("click", function () {
+    const btn1 = num1.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num2 = document.getElementById('num4')
+
+num2.addEventListener("click", function () {
+    const btn1 = num2.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num3 = document.getElementById('num5')
+num3.addEventListener("click", function () {
+    const btn1 = num3.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num4 = document.getElementById('num6')
+num4.addEventListener("click", function () {
+    const btn1 = num4.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num5 = document.getElementById('num7')
+num5.addEventListener("click", function () {
+    const btn1 = num5.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num6 = document.getElementById('num8')
+num6.addEventListener("click", function () {
+    const btn1 = num6.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num7 = document.getElementById('num9')
+num7.addEventListener("click", function () {
+    const btn1 = num7.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num8 = document.getElementById('num0')
+num8.addEventListener("click", function () {
+    const btn1 = num8.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+const num9 = document.getElementById('num.')
+num9.addEventListener("click", function () {
+    const btn1 = num9.textContent
+    const get = document.getElementById('set')
+    get.value = btn1
+
+})
+
