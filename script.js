1f91d87adaf93fe7508923685923498915a72046
Fawad ALi <fawadalichaudhary97@gmail.com>
2026-02-26 12:39:23 +0500

    fadi

--- script.js
+++ script.js
@@ -1,115 +1,98 @@
-const btn = document.getElementById('num1')
+const btn = document.getElementById("num1");
 
 btn.addEventListener("click", function () {
-    const btn1 = btn
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num = document.getElementById('num2')
+    const btn1 = btn;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num = document.getElementById("num2");
 
 num.addEventListener("click", function () {
-    const btn1 = num
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num1 = document.getElementById('num3')
+    const btn1 = num;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num1 = document.getElementById("num3");
 
 num1.addEventListener("click", function () {
-    const btn1 = num1
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num2 = document.getElementById('num4')
+    const btn1 = num1;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num2 = document.getElementById("num4");
 
 num2.addEventListener("click", function () {
-    const btn1 = num2
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num3 = document.getElementById('num5')
+    const btn1 = num2;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num3 = document.getElementById("num5");
 num3.addEventListener("click", function () {
-    const btn1 = num3
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num4 = document.getElementById('num6')
+    const btn1 = num3;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num4 = document.getElementById("num6");
 num4.addEventListener("click", function () {
-    const btn1 = num4
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num5 = document.getElementById('num7')
+    const btn1 = num4;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num5 = document.getElementById("num7");
 num5.addEventListener("click", function () {
-    const btn1 = num5
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num6 = document.getElementById('num8')
+    const btn1 = num5;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num6 = document.getElementById("num8");
 num6.addEventListener("click", function () {
-    const btn1 = num6
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num7 = document.getElementById('num9')
+    const btn1 = num6;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num7 = document.getElementById("num9");
 num7.addEventListener("click", function () {
-    const btn1 = num7
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num8 = document.getElementById('num0')
+    const btn1 = num7;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num8 = document.getElementById("num0");
 num8.addEventListener("click", function () {
-    const btn1 = num8
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num9 = document.getElementById('num.')
+    const btn1 = num8;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num9 = document.getElementById("num.");
 num9.addEventListener("click", function () {
-    const btn1 = num9
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num10 = document.getElementById('+')
+    const btn1 = num9;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num10 = document.getElementById("+");
 num10.addEventListener("click", function () {
-    const btn1 = num10
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num11 = document.getElementById('-')
+    const btn1 = num10;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num11 = document.getElementById("-");
 num11.addEventListener("click", function () {
-    const btn1 = num11
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num12 = document.getElementById('*')
+    const btn1 = num11;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num12 = document.getElementById("*");
 num12.addEventListener("click", function () {
-    const btn1 = num12
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num13 = document.getElementById('/')
+    const btn1 = num12;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num13 = document.getElementById("/");
 num13.addEventListener("click", function () {
-    const btn1 = num13
-    const get = document.getElementById('set')
-    get.value += btn1.textContent
-
-})
-const num14 = document.getElementById('c')
+    const btn1 = num13;
+    const get = document.getElementById("set");
+    get.value += btn1.textContent;
+});
+const num14 = document.getElementById("c");
 num14.addEventListener("click", function () {
-  document.getElementById("set").value = "";
-
-})
-
+    document.getElementById("set").value = "";
+});
